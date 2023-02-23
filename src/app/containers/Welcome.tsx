916aafac3decb3ef6e040035fa43db7863bfb831
santiagoarias2000 <santiago.ariasm@gmail.com>
2023-02-23 12:03:04 -0500

    commit 2

--- src/app/containers/Welcome.tsx
+++ src/app/containers/Welcome.tsx
@@ -1,9 +1,28 @@
-export const Welcome =()=>{
-    return(
+export const Welcome = () => {
+  return (
+    <div className="containner-welcome">
+      <section className="img">
+        <img src="" alt="Imagen de Elecciones 2022" />
+      </section>
 
-        <div className="Cotanner-welcome-title">
-            <h1>Tabla de Contenido</h1>
-            <h2>Información Estadística Publicada</h2>
+      <section className="cards">
+        <div className="container text-center">
+          <div className="row">
+            <div className="col">
+              <div className="card text-center">
+                <div className="card-body">
+                  <h5 className="card-title">Presidenciales</h5>
+                  <a href="#" className="btn btn-primary">
+                    Ver resultados
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col">Column</div>
+            <div className="col">Column</div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
