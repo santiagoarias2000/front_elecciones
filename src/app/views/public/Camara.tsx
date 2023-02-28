cbc70619f9ef01546d6bb628e7747c46133486fe
Felipe <luis.chaparroh@usantoto.edu.co>
2023-02-28 14:27:18 -0500

    Cambios de JuanPablo

--- src/app/views/public/Camara.tsx
+++ src/app/views/public/Camara.tsx
@@ -7,8 +7,9 @@ import camara from "../../../assets/image/camara.jpg";
 export const Camara = () => {
   const [arrayVotesCamaraTerritorial, setArrayVotosCamaraTerritorial] =
     useState<VotesCongreso[]>([]);
-    const [arrayVotesCamaraIndigena, setArrayVotosCamaraIndigena] =
-    useState<VotesCongreso[]>([]);
+  const [arrayVotesCamaraIndigena, setArrayVotosCamaraIndigena] = useState<
+    VotesCongreso[]
+  >([]);
 
   const getVotosCamaraTerritorialCareBackend = async () => {
     const result = await ServicePrivate.requestGET(ApiBack.CAMARA_TERRITORIAL);
@@ -25,16 +26,20 @@ export const Camara = () => {
   }, []);
 
   return (
-    
     <main id="main" className="main">
-      
       <img
-          src={camara}
-          style={{ width: "100%" , maxHeight:"80%", marginTop:"10vw"}}
-          alt="logo principal para la parte superior de la pagina web"
-        />
+        src={camara}
+        style={{
+          width: "100%",
+          maxHeight: "80%",
+          marginTop: "10vw",
+          borderRadius: "5px 5px 0 0",
+          boxShadow: "0px 0 20px #052851",
+        }}
+        alt="logo principal para la parte superior de la pagina web"
+      />
       {/* Navegación estilo breadcrumb: Inicio */}
-     
+
       {/* Navegación estilo breadcrumb: Fin */}
 
       {/* Ejemplo de una tabla para presentación de datos: Inicio */}
@@ -54,35 +59,37 @@ export const Camara = () => {
               <b>TERRITORIAL DEPARTAMENTAL</b>
             </div>
           </div>
-          <table
-            className="colorTable table table-hover"
-            style={{ background: "#05285190 !important" }}
-          >
-            <thead>
-              <tr>
-                <th className="text-center" style={{ width: "40%" }}>
-                  DEPARTAMENTO
-                </th>
-                <th className="text-center" style={{ width: "30%" }}>
-                  ROLE
-                </th>
-                <th className="text-center" style={{ width: "30%" }}>
-                  TOTAL VOTOS
-                </th>
-              </tr>
-            </thead>
-            <tbody className="color">
-              {arrayVotesCamaraTerritorial.map((myVotes, contador) => (
-                <tr key={contador}>
-                  <td className="text-center">
-                    <b>{myVotes.department.nameDepartment}</b>
-                  </td>
-                  <td className="text-center">{myVotes.description_role}</td>
-                  <td className="text-center">{myVotes.votos}</td>
+          <div className="table-wrapper-scroll-y my-custom-scrollbar">
+            <table
+              className="colorTable table table-hover"
+              style={{ background: "#05285190 !important" }}
+            >
+              <thead>
+                <tr>
+                  <th className="text-center" style={{ width: "40%" }}>
+                    DEPARTAMENTO
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    ROLE
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    TOTAL VOTOS
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="color">
+                {arrayVotesCamaraTerritorial.map((myVotes, contador) => (
+                  <tr key={contador}>
+                    <td className="text-center">
+                      <b>{myVotes.department.nameDepartment}</b>
+                    </td>
+                    <td className="text-center">{myVotes.description_role}</td>
+                    <td className="text-center"><b>{myVotes.votos}</b></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
@@ -102,35 +109,87 @@ export const Camara = () => {
               <b>TERRITORIAL INDIGENA</b>
             </div>
           </div>
-          <table
-            className="colorTable table table-hover"
-            style={{ background: "#05285190 !important" }}
-          >
-            <thead>
-              <tr>
-                <th className="text-center" style={{ width: "40%" }}>
-                  DEPARTAMENTO
-                </th>
-                <th className="text-center" style={{ width: "30%" }}>
-                  ROLE
-                </th>
-                <th className="text-center" style={{ width: "30%" }}>
-                  TOTAL VOTOS
-                </th>
-              </tr>
-            </thead>
-            <tbody className="color">
-              {arrayVotesCamaraIndigena.map((myVotes, contador) => (
-                <tr key={contador}>
-                  <td className="text-center">
-                    <b>{myVotes.department.nameDepartment}</b>
-                  </td>
-                  <td className="text-center">{myVotes.description_role}</td>
-                  <td className="text-center">{myVotes.votos}</td>
+          <div className="table-wrapper-scroll-y my-custom-scrollbar">
+            <table
+              className="colorTable table table-hover"
+              style={{ background: "#05285190 !important" }}
+            >
+              <thead>
+                <tr>
+                  <th className="text-center" style={{ width: "40%" }}>
+                    DEPARTAMENTO
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    ROLE
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    TOTAL VOTOS
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="color">
+                {arrayVotesCamaraIndigena.map((myVotes, contador) => (
+                  <tr key={contador}>
+                    <td className="text-center">
+                      <b>{myVotes.department.nameDepartment}</b>
+                    </td>
+                    <td className="text-center">{myVotes.description_role}</td>
+                    <td className="text-center"><b>{myVotes.votos}</b></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div className="col-lg-12" style={{ color: "#052851 !important" }}>
+        <div className="cardBorder card">
+          <div
+            className="container-fluid display-flex justify-content-center"
+            style={{
+              background: "#052851",
+              color: "#FFFFFF",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div className="text-center">
+              <b>TERRITORIAL INDIGENA</b>
+            </div>
+          </div>
+          <div className="table-wrapper-scroll-y my-custom-scrollbar">
+            <table
+              className="colorTable table table-hover"
+              style={{ background: "#05285190 !important" }}
+            >
+              <thead>
+                <tr>
+                  <th className="text-center" style={{ width: "40%" }}>
+                    DEPARTAMENTO
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    ROLE
+                  </th>
+                  <th className="text-center" style={{ width: "30%" }}>
+                    TOTAL VOTOS
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="color">
+                {arrayVotesCamaraIndigena.map((myVotes, contador) => (
+                  <tr key={contador}>
+                    <td className="text-center">
+                      <b>{myVotes.department.nameDepartment}</b>
+                    </td>
+                    <td className="text-center">{myVotes.description_role}</td>
+                    <td className="text-center"><b>{myVotes.votos}</b></td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
       {/* Ejemplo de una tabla para presentación de datos: Fin */}
