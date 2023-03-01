1533ee141f467a99f28b6df93bb873f40d83f17b
juanrobles21 <juan.roblesa@usantoto.edu.co>
2023-02-28 21:43:13 -0500

    Update model VotesCongreso view data

--- src/app/utilities/domains/ApiBack.tsx
+++ src/app/utilities/domains/ApiBack.tsx
@@ -5,5 +5,7 @@ const ApiBack = {
   CAMARA_TERRITORIAL: "/api/public/camara/territorial",
   CAMARA_INDIGENA: "/api/public/camara/indigena",
   CAMARA_AFRODESCENDIENTE: "/api/public/camara/afrodescendiente",
+
+  CAMARA_TERRITORIAL_DEPARTAMENTO:"/api/public/camara/circuncripcion/territorial/departamento",
 };
 export default ApiBack;
--- src/app/utilities/routes/InternalRouting.tsx
+++ src/app/utilities/routes/InternalRouting.tsx
@@ -2,13 +2,16 @@ import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "../../containers/Welcome";
 import { MainBoard } from "../../containers/MainBoard";
-import { Camara } from "../../views/public/Camara";
+import { Camara } from "../../views/public/camara/Camara";
+import { CamaraTerritorialDepartamento } from "../../views/public/camara/CamaraTerritorialDepartamento";
 
 
 //Lazy structure from use the all tsx
 
 const LazyWelcome =lazy(()=>import("../../containers/Welcome").then(()=>({default:Welcome})));
-const LazyCamara =lazy(()=>import("../../views/public/Camara").then(()=>({default:Camara})));
+const LazyCamara =lazy(()=>import("../../views/public/camara/Camara").then(()=>({default:Camara})));
+const LazyCamaraTerritorialDepartamento =lazy(()=>import("../../views/public/camara/CamaraTerritorialDepartamento").then(()=>({default:CamaraTerritorialDepartamento})));
+
 
 
 export const InternalRouting = () => {
@@ -17,6 +20,7 @@ export const InternalRouting = () => {
       //Routes Default
       <Route path="/welcome" element={<LazyWelcome />} />
       <Route path="/camara" element={<LazyCamara />} />
+      <Route path="/camara/circuncripcion/territorial/departamento/:idDepartment" element={<LazyCamaraTerritorialDepartamento />} />
       
       
     </Routes>
