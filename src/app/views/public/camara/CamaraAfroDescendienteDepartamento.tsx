f158bd548350a3875b7a68a217942cc6ec0c72ef
juanrobles21 <juan.roblesa@usantoto.edu.co>
2023-03-09 14:19:49 -0500

    Combo box con scroll municipio

--- src/app/views/public/camara/CamaraAfroDescendienteDepartamento.tsx
+++ src/app/views/public/camara/CamaraAfroDescendienteDepartamento.tsx
@@ -72,61 +72,69 @@ export const CamaraAfroDescendienteDepartamento = () => {
       {/* Navegación estilo breadcrumb: Fin */}
 
       {/* Ejemplo de una tabla para presentación de datos: Inicio */}
-      <div className="col-lg-12" style={{ color: "#052851 !important" }}>
+      <div className="col-lg-12" style={{ color: "#052851 !important" }}></div>
         <div className="cardBorder card">
           <div className="container-fluid display-flex justify-content-center container_title">
             <div className="text-center">
               <b className="title_table">TERRITORIAL AFRO-DESCENDIENTE</b>
             </div>
           </div>
-          <div className="container text-center">
-            <div className="row">
-              <div className="col align-content-center my-3">
-                <a
+          <div className="container">
+          <div className="row">
+            <div className="col-sm ">
+              <div className="dropdown align-content-center my-3">
+                <button
+                  type="button"
                   className="buttonBack buttonBack-primary dropdown-toggle"
-                  role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   Municipios
-                </a>
-                <ul className="dropdown-menu">
-                  {arrayMunicipio.map((myMunicipality, indice) => (
-                    <a
-                      href={
-                        "/guiaelectoral/camara/circuncripcion/afrodescendiente/departamento/" +
-                        myMunicipality.id_department +
-                        "/municipio/" +
-                        myMunicipality.id_municipality
-                      }
-                    >
-                      <li>
-                        <a className="dropdown-item">
-                          {myMunicipality.name_municipality}
-                        </a>
-                      </li>
-                    </a>
-                  ))}
+                </button>
+                <ul
+                  className="dropdown-menu selectpicker"
+                  data-live-search="true"
+                  style={{ maxHeight: "200px", overflowY: "auto" }}
+                >
+                  <li>
+                    {arrayMunicipio.map((myMunicipality, indice) => (
+                      <a
+                        className="dropdown-item"
+                        href={
+                          "/guiaelectoral/camara/circuncripcion/territorial/departamento/" +
+                          myMunicipality.id_department +
+                          "/municipio/" +
+                          myMunicipality.id_municipality
+                        }
+                      >
+                        {myMunicipality.name_municipality}
+                      </a>
+                    ))}
+                  </li>
                 </ul>
               </div>
-              <div className="col">
-                <h5 className="text-center my-4" style={{ color: "#052851" }}>
-                  {arrayDepartamento.map((myDepartment) => (
-                    <b>{myDepartment.name_department}</b>
-                  ))}
-                </h5>
-              </div>
-              <div className="col">
-                <Form>
-                  <InputGroup className="my-3">
-                    <Form.Control
-                      onChange={(e) => setSearch(e.target.value)}
-                      placeholder="Search Keeper"
-                      style={{ textAlign: "right", marginRight: "5px" }}
-                    ></Form.Control>
-                  </InputGroup>
-                </Form>
-              </div>
+            </div>
+            <div className="col">
+              <h5 className="text-center my-4" style={{ color: "#052851" }}>
+                {arrayDepartamento.map((myDepartment) => (
+                  <b>{myDepartment.name_department}</b>
+                ))}
+              </h5>
+            </div>
+            <div className="col-sm">
+              <Form id="form_conta">
+                <InputGroup className="my-3 container_form">
+                  <Form.Control
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Buscar partido político"
+                    style={{ textAlign: "right", marginRight: "5px" }}
+                  ></Form.Control>
+                </InputGroup>
+              </Form>
+            </div>
+          </div>
+        </div>
+
 
               <div className="table-wrapper-scroll-y my-custom-scrollbar">
                 <table
@@ -157,10 +165,10 @@ export const CamaraAfroDescendienteDepartamento = () => {
                       })
                       .map((myVotes, contador) => (
                         <tr key={contador}>
-                          <td className="text-center">
+                          <td className="text-left">
                             <b>{myVotes.candidate_name}</b>
                           </td>
-                          <td className="text-center">
+                          <td className="text-left">
                             {myVotes.description_politicparty}
                           </td>
                           <td className="text-center">{myVotes.votos}</td>
@@ -184,8 +192,7 @@ export const CamaraAfroDescendienteDepartamento = () => {
                       type="button"
                       className="buttonBack buttonBack-primary"
                     >
-                      <a className="link_hitdata">
-                        <Link to={"/guiaelectoral/camara/"}></Link>
+                      <a className="link_hitdata" href="/guiaelectoral/camara/">
                         <i className="bi bi-arrow-left-circle"></i>
                         &nbsp;&nbsp;REGRESAR A ELEGIR DEPARTAMENTO
                       </a>
@@ -194,10 +201,6 @@ export const CamaraAfroDescendienteDepartamento = () => {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-
       {/* Ejemplo de una tabla para presentación de datos: Fin */}
     </main>
   );
