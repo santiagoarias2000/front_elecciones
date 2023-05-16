dfab92e7bd0814aaed80f8189d3d8ddd8ca72e15
Andres <andres.cardenasa@usantoto.edu.co>
2023-05-16 11:25:13 -0500

    stage data index

--- src/app/containers/Welcome.tsx
+++ src/app/containers/Welcome.tsx
@@ -11,7 +11,6 @@ import ImgDonJose from "../../assets/image/Patrocinadores/DonJose.webp";
 import { Carousel, Modal } from "react-bootstrap";
 import "../../assets/js/carrusel.js";
 import { useState, useEffect } from "react";
-import { useEpayco } from 'react-epayco';
 import ImageSpinner from "../../assets/image/Blog/publicidadloteria.webp";
 
 export const Welcome = () => {
@@ -26,36 +25,6 @@ export const Welcome = () => {
   useEffect(() => {
     hideModal();
   }, []);
-
-  var data = {
-    //Parametros compra (obligatorio)
-    name: "Vestido Mujer Primavera",
-    description: "Vestido Mujer Primavera",
-    invoice: "12333322223",
-    currency: "cop",
-    amount: "12000",
-    tax_base: "0",
-    tax: "0",
-    country: "co",
-    lang: "es",
-
-    //Onpage="false" - Standard="true"
-    external: "false",
-
-    //Atributos opcionales
-    extra1: "extra1",
-    extra2: "extra2",
-    extra3: "extra3",
-    // confirmation: "http://secure2.payco.co/confirmation.php",
-    //response: "http://secure2.payco.co/response.php",
-
-    //Atributos cliente
-    name_billing: "Andres Perez",
-    address_billing: "Carrera 19 numero 14 91",
-    type_doc_billing: "cc",
-    mobilephone_billing: "3050000000",
-    number_doc_billing: "100000000",
-  };
 
   return (
     <div id="about" className="containner-welcome">
